6eae2b2c47cc5d4d2b3a8380e0417c7658dee7c0
xsalles <xs.salles@gmail.com>
2025-01-28 23:06:41 -0300

    feat: added configModule to work with a lot of smaller modules and added enviroments variable in a zod schema.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateUserDto } from './dto/create-account.dto'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validate: (env) => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreateAccountController],
   providers: [PrismaService, CreateUserDto],
 })
